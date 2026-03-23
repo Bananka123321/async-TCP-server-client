221872c2bc28bdbb63d32a71ce54c724c358a068
Bananka123321 <vanushabrend@gmail.com>
2026-03-23 21:04:17 +0700

    Removed unnecessary files from Git tracking

--- GUI/build/Windows-release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GUI_FILE Z:/TPU/Education Project/Ivan_messanger/GUI/build/Windows-release/GUI.exe)
-set(__QT_DEPLOY_TARGET_GUI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GUI_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
